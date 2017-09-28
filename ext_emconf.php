9cd2eb2820f564999beaf401bcc0afcb49581df5
Franz Kugelmann <franz.kugelmann@elementare-teilchen.de>
2017-09-28 07:20:26 +0200

    [FEATURE] add proper dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -19,7 +19,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '0.0.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3'=>'8.6.0-0.0.0',
+			'typo3' => '8.7.0-0.0.0',
+            'typo3_console' => '4.6.6-0.0.0',
 		),
 		'conflicts' => array(
 		),
