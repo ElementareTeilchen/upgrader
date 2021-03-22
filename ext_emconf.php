5f70018321cdc703281a9ab3281b9217a42bc8e9
Sebastian Müller <sebastian.mueller@elementare-teilchen.de>
2021-03-22 09:56:05 +0100

    Remove typo3_console extension depends constraint (since now in vendor and no extension anymore)

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,6 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '8.7.0-0.0.0',
-            'typo3_console' => '4.6.6-0.0.0',
 		),
 		'conflicts' => array(
 		),
