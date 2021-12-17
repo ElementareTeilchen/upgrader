d5c071aa873c8af0319bd340080228cc80b685b1
Franz Kugelmann <franz.kugelmann@elementare-teilchen.de>
2021-12-17 13:07:53 +0100

    [TASK] reduce console output of cleanup

--- 9to10/cleanup.sh
+++ 9to10/cleanup.sh
@@ -14,47 +14,47 @@ echo -e '* start cleanup in ${PROJECT_ROOT}'
 date
 echo -e '** cleanup:deletedrecords'
 echo -e 'for big so far badly maintained systems right upfront: start with deleted to get rid of those deleted records. Nearly all other tasks start with full pagetree (including deleted records)';
-${TYPO3_CORE_BIN} cleanup:deletedrecords
+${TYPO3_CORE_BIN} -q cleanup:deletedrecords
 date
 
 # now as suggested in typo3_src/typo3/sysext/lowlevel/README.rst
 echo -e '** cleanup:orphanrecords'
-${TYPO3_CORE_BIN} cleanup:orphanrecords
+${TYPO3_CORE_BIN} -q cleanup:orphanrecords
 date
 
 echo -e '** cleanup:multiplereferencedfiles --update-refindex'
-${TYPO3_CORE_BIN} cleanup:multiplereferencedfiles --update-refindex
+${TYPO3_CORE_BIN} -q cleanup:multiplereferencedfiles --update-refindex
 date
 
 echo -e '** cleanup:deletedrecords'
-${TYPO3_CORE_BIN} cleanup:deletedrecords
+${TYPO3_CORE_BIN} -q cleanup:deletedrecords
 date
 
 
 echo -e '** cleanup:missingrelations --update-refindex'
-${TYPO3_CORE_BIN} cleanup:missingrelations --update-refindex
+${TYPO3_CORE_BIN} -q cleanup:missingrelations --update-refindex
 date
 
 echo -e '** cleanup:flexforms'
-${TYPO3_CORE_BIN} cleanup:flexforms
+${TYPO3_CORE_BIN} -q cleanup:flexforms
 date
 
 # just point out missing files, but do NOT remove reference to it
 echo -e '** cleanup:missingfiles --dry-run --update-refindex'
-${TYPO3_CORE_BIN} cleanup:missingfiles --dry-run --update-refindex
+${TYPO3_CORE_BIN} -q cleanup:missingfiles --dry-run --update-refindex
 date
 
 echo -e '** cleanup:lostfiles --update-refindex'
-${TYPO3_CORE_BIN} cleanup:lostfiles --update-refindex
+${TYPO3_CORE_BIN} -q cleanup:lostfiles --update-refindex
 date
 
 # and again in case the above commands freeed some more stuff
 echo -e '** cleanup:orphanrecords'
-${TYPO3_CORE_BIN} cleanup:orphanrecords
+${TYPO3_CORE_BIN} -q cleanup:orphanrecords
 date
 
 echo -e '** cleanup:deletedrecords'
-${TYPO3_CORE_BIN} cleanup:deletedrecords
+${TYPO3_CORE_BIN} -q cleanup:deletedrecords
 date
 
 echo -e '** done!!!'
