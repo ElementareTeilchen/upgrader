7a5c82091d4a584e88b7ce5fad8d99b4f9093202
Franz Kugelmann <franz.kugelmann@elementare-teilchen.de>
2022-05-11 19:22:38 +0200

    add cleanup for corrupt pages language connections

--- 10to11/sql/common/preUpgradeRun.sql
+++ 10to11/sql/common/preUpgradeRun.sql
@@ -2,3 +2,7 @@
 -- alter table `sys_refindex` drop index lookup_string;
 -- ALTER TABLE sys_refindex CHANGE ref_string `ref_string` VARCHAR(1024) DEFAULT '' NOT NULL;
 -- CREATE INDEX `lookup_string` ON sys_refindex (ref_string(255));
+
+-- there should not be pages in default language having l10n_parent set
+-- might break i.e. sitemap rendering
+update pages set l10n_parent = 0  where sys_language_uid = 0;
