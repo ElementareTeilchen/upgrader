4fc74a5d22131b3e306314f3a4803005b812e5b3
Franz Kugelmann <franz.kugelmann@elementare-teilchen.de>
2022-07-21 16:36:22 +0200

    [TASK] Remove old error entries from sys_log

--- 10to11/sql/common/reduceSysLog.sql
+++ 10to11/sql/common/reduceSysLog.sql
@@ -3,3 +3,4 @@ delete FROM `sys_log` where details like '[scheduler%';
 delete FROM `sys_log` where details like 'User %s logged in from%';
 delete FROM `sys_log` where tstamp < UNIX_TIMESTAMP(DATE_SUB(NOW(), INTERVAL 360 DAY));
 delete FROM `sys_log` where details like '%was deleted unrecoverable%';
+delete FROM `sys_log` where error=1;
