a88c227151c4999f0408e87ab75a6ded4aae754a
Franz Kugelmann <franz.kugelmann@elementare-teilchen.de>
2024-09-09 14:52:52 +0200

    [TASK] remove further entries from sys_log

--- v12/sql/common/reduceSysLog.sql
+++ v12/sql/common/reduceSysLog.sql
@@ -1,6 +1,9 @@
 delete FROM `sys_log` where details like '%has cleared the cache%';
 delete FROM `sys_log` where details like '[scheduler%';
 delete FROM `sys_log` where details like 'User %s logged in from%';
+delete FROM `sys_log` where details like 'User %s logged out from%';
+delete FROM `sys_log` where details like 'Login-attempt from%';
+delete FROM `sys_log` where details like '%switched to user%';
 delete FROM `sys_log` where tstamp < UNIX_TIMESTAMP(DATE_SUB(NOW(), INTERVAL 360 DAY));
 delete FROM `sys_log` where details like '%was deleted unrecoverable%';
 -- errors from old version should not be relevant in new version
