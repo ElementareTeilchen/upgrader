73553853e88ee91a609a82fd5a3899339af2736c
Franz Kugelmann <franz.kugelmann@elementare-teilchen.de>
2024-09-09 14:54:44 +0200

    [TASK] remove blocking duplicates from sys_category_record_mm

--- v12/sql/common/preUpgradeRun.sql
+++ v12/sql/common/preUpgradeRun.sql
@@ -6,3 +6,13 @@
 -- there should not be pages in default language having l10n_parent set
 -- might break i.e. sitemap rendering
 update pages set l10n_parent = 0  where sys_language_uid = 0;
+
+-- remove useless duplicates to allow DB compare to create new indexes
+ALTER TABLE sys_category_record_mm ADD COLUMN temp SERIAL;
+DELETE t1 FROM sys_category_record_mm t1, sys_category_record_mm t2
+WHERE t1.temp > t2.temp
+  AND t1.uid_local = t2.uid_local
+  AND t1.uid_foreign = t2.uid_foreign
+  AND t1.tablenames = t2.tablenames
+  AND t1.fieldname = t2.fieldname;
+ALTER TABLE sys_category_record_mm DROP COLUMN temp;
