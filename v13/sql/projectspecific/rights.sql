a7844f619001303bad17c22a266711745ead2b5c
Franz Kugelmann <franz.kugelmann@elementare-teilchen.de>
2024-11-06 15:17:54 +0100

    [TASK] keep new web_info (sub-)modules allowed for editors

--- v13/sql/projectspecific/rights.sql
+++ v13/sql/projectspecific/rights.sql
@@ -1,1 +1,5 @@
 -- fill in whatever you need for your project
+
+-- since v12 the submodules must be checked as own modules
+update be_groups set groupMods = replace(groupMods, 'web_info,', 'web_info,web_info_overview,web_info_translations,');
+-- update be_groups set groupMods = replace(groupMods, 'web_brofix,', 'web_brofix,web_brofix_broken_links,web_brofix_manage_exclusions,');
